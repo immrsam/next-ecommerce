f620f0175ff4aadfe4bcb4a3a73595a4892a7a39
Sam <sam.bradbury@hotmail.com>
2025-01-04 22:59:59 +1100

    further work on menu componenets

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,28 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Image
+        src="/menu.png"
+        alt=""
+        width={28}
+        height={28}
+        className="cursor-pointer"
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className="">
+          <Link href="/">Home</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Menu;
