01f2a7861fc9f0e2f31048226f1d37c5b8954ed5
Sam <sam.bradbury@hotmail.com>
2025-01-06 21:40:46 +1100

    menu searchbar and icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
 import Menu from "./Menu";
+import NavIcons from "./NavIcons";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-12 xl:px-32 2xl:px-64 relative">
       {/** mobile */}
-      <div className="h-full flex items-center justify-between">
+      <div className="flex md:hidden h-full items-center justify-between">
         <Link href="/" className="">
           <div className="text-2xl tracking-wide">SJB</div>
         </Link>
         <Menu />
+      </div>
+      {/* larger screens */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/* LEFT */}
+        <div className="w-1/3">
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">SJB</div>
+          </Link>
+        </div>
+        {/* RIGHT */}
+        <div className="w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
+        </div>
       </div>
     </div>
   );
