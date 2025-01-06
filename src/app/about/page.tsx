29345597910df88e6b3295d507226b141a63912f
Sam <sam.bradbury@hotmail.com>
2025-01-06 23:11:39 +1100

    added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+  return (
+    <div className="">
+      <h1>ABOUT</h1>
+      <ul>
+        <li>My</li>
+        <li>Web</li>
+        <li>Site</li>
+      </ul>
+    </div>
+  );
+};
+
+export default AboutPage;
